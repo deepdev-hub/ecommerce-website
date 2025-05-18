5728473bfb94a6e93f1cd823721d5973b682aaad
tunguyen2604 <tu.nht235858@sis.hust.edu.vn>
2025-05-18 21:16:29 +0700

    Update

--- src/main/java/com/example/ecommerce/model/admin.java
+++ src/main/java/com/example/ecommerce/model/admin.java
@@ -1,9 +1,11 @@
 package com.example.ecommerce.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Admin {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)

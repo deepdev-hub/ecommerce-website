8e618d1d00ba6ea7e79da8df48203f2322cbe821
kjfvnju <hoangvuhuy.3006@gmail.com>
2025-06-01 17:58:22 +0700

    minor bug

--- src/main/java/com/example/ecommerce/model/Stationary.java
+++ src/main/java/com/example/ecommerce/model/Stationary.java
@@ -3,10 +3,12 @@ package com.example.ecommerce.model;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@EqualsAndHashCode(callSuper=false)
 @AllArgsConstructor
 @NoArgsConstructor
 public class Stationary extends Product {    

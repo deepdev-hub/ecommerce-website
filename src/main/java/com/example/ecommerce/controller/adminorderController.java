809fc2cb8a3f5795566f5d03a575ad4c80d35834
kjfvnju <hoangvuhuy.3006@gmail.com>
2025-06-04 21:55:12 +0700

    !

--- src/main/java/com/example/ecommerce/controller/adminorderController.java
+++ src/main/java/com/example/ecommerce/controller/adminorderController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class AdminorderController {
+public class AdminOrderController {
     @GetMapping("/admin-order")
     public String adminOrder() {
         return "admin-order";
